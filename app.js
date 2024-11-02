8ff6d07d34067a2267d78bd8d480dfe96afe819d
natecross123 <137192717+natecross123@users.noreply.github.com>
2024-11-02 16:36:59 -0500

    Excercise3

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 window.addEventListener('load', () => {
     document.querySelector("button#btn").addEventListener("click", (event) => {
         event.preventDefault();
+        const searchField = document.getElementById('searchField').value.trim(); 
+        const results = document.getElementById('results'); 
 
-        fetch('superheroes.php')
-            .then(response => response.text())
+        let url = 'superheroes.php';
+        if (searchField !== "") {
+            
+            url += `?query=${encodeURIComponent(searchField)}`;
+        }
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
             .then(data => {
                
-                alert(data);
+                results.innerHTML = data; 
             })
-            .catch(error => console.error('Error:', error));
+            .catch(error => {
+                console.error('Error:', error);
+                results.innerHTML = "<span>Error fetching data. Please try again.</span>";
+            });
     });
 });
